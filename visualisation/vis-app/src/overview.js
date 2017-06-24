899af7e02fa5d2f9d5620d87010986023689e393
Johannes Huber <huber.johannes@online.de>
2017-06-25 01:02:02 +0200

    Visualisation: Add Sorting in overview

--- visualisation/vis-app/src/overview.js
+++ visualisation/vis-app/src/overview.js
@@ -6,13 +6,18 @@ export class Overview {
     this.treatments = [];
     this.all_treatments = [];
     this.filter = "";
+    this.order = -1;
+    this.key = "last_year_cnt";
+    this.sort_label = "Frequency";
   }
 
   activate() {
+    var self = this;
     return this.client.fetch('http://localhost:8765/treatment_summary')
     .then(response => response.json())
     .then(data => {
-      this.treatments = this.all_treatments = data.treatment_list;
+      this.treatments = this.all_treatments = data.treatment_list.sort(
+        function(a, b){return (a[self.key] - b[self.key]) * self.order})
     });
   }
 
@@ -29,5 +34,30 @@ export class Overview {
     else{
       self.treatments = self.all_treatments;
     }
+  }
+
+  sort() {
+    var self = this;
+    function compare(a, b) {
+      if(self.key == "treatment") return a.treatment.localeCompare(b.treatment) * self.order;
+      return (a[self.key] - b[self.key]) * self.order;
+    };
+    this.treatments.sort(compare);
+    this.all_treatments.sort(compare);
+  }
+
+  changeOrder() {
+    this.order *= -1;
+    this.sort();
+  }
+
+  changeKey(new_key) {
+    this.key = new_key;
+    this.sort();
+    this.sort_label = {
+      last_year_cnt: "Frequency",
+      total_score: "Score",
+      treatment: "Name"
+    }[new_key];
   }
 }
