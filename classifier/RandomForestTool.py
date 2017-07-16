c126fa3304de63a82d7a8fa0db56d684e0ed96ff
sourabhDandage <sourabh.dandage@st.ovgu.de>
2017-07-16 23:51:42 +0200

    removed unuded code

--- classifier/RandomForestTool.py
+++ classifier/RandomForestTool.py
@@ -4,21 +4,15 @@ from sklearn.ensemble import RandomForestClassifier
 # Load pandas
 import pandas as pd
 
-import nltk
-import string
-
 # Load numpy
 import numpy as np
-import  csv
-from nltk.corpus import stopwords
 from textblob import TextBlob
 from featureAnalyzer import feat_analyser as sn
 
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 import sklearn
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
+
 #end of method
 data_dir = '../data'
 
@@ -56,25 +50,6 @@ df_test_set = df_test_set.filter(['post_id', 'sentence','treatments', 'sentiment
 
 
 
-#2. TF-IDF
-#Train Data
-"""
-vectorizer = TfidfVectorizer(min_df=1)
-tfidf = vectorizer.fit_transform(df_train_set["sentence"].map(str) + "'\n'" + df_train_set["after"].map(str) + "'\n'" + df_train_set["third"].map(str)).toarray()
-tfidf_train_df = pd.DataFrame(tfidf, columns=[("tfidf_" + str(i)) for i in range(len(tfidf[0]))])
-df_train_set = pd.concat((df_train_set, tfidf_train_df), axis=1)
-
-#print(vectorizer.vocabulary)
-print(tfidf)
-#print(df)
-
-#Test Data
-tfidf2 = vectorizer.transform(df_test_set["sentence"].map(str) + "'\n'" + df_test_set["after"].map(str) + "'\n'" + df_test_set["third"].map(str)).toarray()
-tfidf_test_df = pd.DataFrame(tfidf2, columns=[("tfidf_" + str(i)) for i in range(len(tfidf2[0]))])
-df_test_set = pd.concat((df_test_set, tfidf_test_df), axis=1)
-
-"""
-
 #3. Polarity Score
 #For Training Data
 df_train_set['polarity'] = pd.Series(TextBlob(x).polarity for x in (df_train_set["sentence"].map(str)))
@@ -83,16 +58,12 @@ df_test_set['polarity'] = pd.Series(TextBlob(x).polarity for x in  (df_test_set[
 #commented on 07-07-2017 by Atin
 #df_final_set['polarity']=pd.Series((TextBlob(x).polarity for x in (df_test_set["sentence"])))
 df_final_set['polarity']=pd.Series((TextBlob(x).polarity for x in (df_final_set["sentence"].map(str))))
-
-
-
 #print(df_final_set.head(2))
 
 
 
 #4. Subjectivity Score
 #For Training Data
-
 df_train_set['subjectivity'] = pd.Series(TextBlob(x).subjectivity for x in (df_train_set["sentence"].map(str)))
 #For Test Data
 df_test_set['subjectivity'] = pd.Series(TextBlob(x).subjectivity for x in (df_test_set["sentence"].map(str)))
@@ -144,6 +115,7 @@ print(df_final_set.head(2))
 # Create a list of the feature column's names
 
 #Putting Final 8 Features 16-07-2017 Atin
+
 features = df_train_set.columns[5:]
 list_feat = list(features)
 list_feat.remove('negative_word_count')
@@ -163,18 +135,6 @@ final_features=pd.DataFrame
 final_features=df_final_set[[ 'first_per_pron', 'sec_per_pron',  'personal_pron', 'total_pron', 'sing_proper_noun', 'trivial_score',  'positive_word_count', 'vander_score']]
 print("Features:",features)
 print("Final fe",final_features.head(50))
-
-#Creating Training and Test Data
-#df_train_set['is_train'] = np.random.uniform(0, 1, len(df_train_set)) <= .75
-#print(df['is_train'])
-
-# Create two new dataframes, one with the training rows, one with the test rows
-#train, test = df_train_set[df_train_set['is_train'] == True], df_train_set[df_train_set['is_train'] == False]
-
-
-#print(train.head())
-#print(test.head())
-
 
 # Show the number of observations for the test and training dataframes
 #print('Number of observations in the training data:', len(df_train_set))
@@ -205,7 +165,6 @@ Y=np.column_stack((y,y2))
 #Changed on 16-07-2017 Atin
 #dict_cls_weight = [{0:1,1:5,2:5},{0:1,1:1}]
 dict_cls_weight ='balanced'
-#Train The Random Forest Classifier
 # Create a random forest classifier. By convention, clf means 'classifier'
 
 sample_wt = sklearn.utils.class_weight.compute_sample_weight(dict_cls_weight,Y)
@@ -225,27 +184,6 @@ clf.fit(df_train_set[features], Y,sample_weight=sample_wt)
 #predict_result =clf.predict(df_test_set[features])
 print("before",final_features.describe())
 
-""" tried this part"""
-"""groups = final_features.groupby(final_features.index // 1000)
-for group in groups:
-    predict_result=clf.predict(group.fillna(0))
-    print("predict", predict_result)
-
-"""
-
-""" #commenting hardcoded mapping
-sentiment_mapping = {
-    2: "pos",
-    0: "neu",
-    1: "neg"
-}
-
-factuality_mapping = {
-    1: "yes",
-    0: "no"
-}
-
-"""
 sentiment_mapping = {i:map_sent[i] for i in range(len(map_sent))}
 factuality_mapping = {i:map_fact[i] for i in range(len(map_fact))}
 
@@ -273,89 +211,3 @@ final_visual.to_csv(data_dir + "/sentences_classified.csv", encoding='utf-8', in
 
 
 
-"""
-#encoded value for sentiment
-A = np.array(predict_result)
-B=np.asmatrix(A)
-C=B[:,0]
-C=C.astype(int)
-print(C)
-
-
-C=C.ravel()
-C = np.asarray(C)
-C=C[0]
-print(C)
-
-#encoded value for factuality
-B=np.asmatrix(A)
-C2=B[:,1]
-C2=C2.astype(int)
-print(C2)
-
-
-C2=C2.ravel()
-C2 = np.asarray(C2)
-C2=C2[0]
-print(C2)
-
-#------------------------------------------------------------------
-# View the predicted probabilities of the first 10 observations
-#predict_result_prob=clf.predict_proba(df_test_set[features])[0:len(df_test_set)]
-#print(predict_result_prob)
-#--------------------------------------------------------------------
-
-#Evaluate classifier
-#predicted values for sentiment
-y_pred = []
-for i in range(len(df_final_set)):
-    print(y_index[C[i]])
-    y_pred.append(y_index[C[i]])
-#print(preds2)
-y_pred =np.asarray(y_pred)
-print(y_pred)
-print("Test ",df_test_set['sentiment'])
-
-
-#predicted values for factual
-y2_pred = []
-for i in range(len(df_final_set)):
-    print(y2_index[C2[i]])
-    y2_pred.append(y2_index[C2[i]])
-#print(preds2)
-y2_pred =np.asarray(y2_pred)
-print(y2_pred)
-print("Test ",df_test_set['factual'])
-
-
-# Create confusion matrix
-confusion_matrix=pd.crosstab(df_test_set['sentiment'], y_pred, rownames=['Actual Sentiment'], colnames=['Predicted Sentiment'],margins=True)
-print(confusion_matrix)
-
-#Precision
-print("Precision: ",precision_score(df_test_set['sentiment'], y_pred,average=None))
-print("Precision avg=micro: ",precision_score(df_test_set['sentiment'], y_pred,average='macro'))
-print("Precision avg=weighted: ",precision_score(df_test_set['sentiment'], y_pred,average='weighted'))
-#Recall
-
-print("Precision: ",recall_score(df_test_set['sentiment'], y_pred,average=None))
-print("Precision avg=micro: ",recall_score(df_test_set['sentiment'], y_pred,average='macro'))
-print("Precision avg=weighted: ",recall_score(df_test_set['sentiment'], y_pred,average='weighted'))
-
-
-
-# Create confusion matrix
-confusion_matrix=pd.crosstab(df_test_set['factual'], y2_pred, rownames=['Actual Factuality'], colnames=['Predicted Factuality'],margins=True)
-print(confusion_matrix)
-
-#Precision
-print("Precision: ",precision_score(df_test_set['factual'], y2_pred,average=None))
-print("Precision avg=micro: ",precision_score(df_test_set['factual'], y2_pred,average='macro'))
-print("Precision avg=weighted: ",precision_score(df_test_set['factual'], y2_pred,average='weighted'))
-
-#Recall
-print("Precision: ",recall_score(df_test_set['factual'], y2_pred,average=None))
-print("Precision avg=micro: ",recall_score(df_test_set['factual'], y2_pred,average='macro'))
-print("Precision avg=weighted: ",recall_score(df_test_set['factual'], y2_pred,average='weighted'))
-
-"""
